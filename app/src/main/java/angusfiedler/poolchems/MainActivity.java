541c73db99c8e309873e68ffa69b979f7f2b49ca
Angus Fiedler <35715801+AngusFiedler@users.noreply.github.com>
2018-01-31 14:44:18 -0700

    Changed sodaRaise into a for loop

--- app/src/main/java/angusfiedler/poolchems/MainActivity.java
+++ app/src/main/java/angusfiedler/poolchems/MainActivity.java
@@ -45,18 +45,16 @@ public class MainActivity extends AppCompatActivity {
                     final double currentPH = Double.parseDouble(currentpH.getText().toString());
                     final double newPH = Double.parseDouble(newpH.getText().toString());
 
-                    phChemical.setText(phChemical(currentPH,newPH));
+                    phChemical.setText(phChemical(currentPH, newPH));
                     String poolCapComma = poolCapacity.getText().toString();
                     final int poolCap = Integer.valueOf(poolCapComma.replaceAll(",", "").toString());
                     //TextView invalid = (TextView) findViewById(R.id.invalid)
                     if (newPH > currentPH) {
                         Result.setText(Double.toString((RoundTo2Decimals(sodaRaisePH(currentPH, newPH, poolCap)))));
-                    }
-                    else if (newPH == currentPH) {
+                    } else if (newPH == currentPH) {
                         Result.setText(Double.toString(0));
-                    }
-                    else {
-                        Result.setText(Double.toString(RoundTo2Decimals(bisulfateLowerPH(currentPH,newPH,poolCap))));
+                    } else {
+                        Result.setText(Double.toString(RoundTo2Decimals(bisulfateLowerPH(currentPH, newPH, poolCap))));
                     }
                 }
 
@@ -89,27 +87,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     double RoundTo2Decimals(double val) {
         DecimalFormat df2 = new DecimalFormat("###.##");
         return Double.valueOf(df2.format(val));
     }
 
 
-
-
-    private String phChemical(double currentph, double newph){
-        if(newph > currentph){
+    private String phChemical(double currentph, double newph) {
+        if (newph > currentph) {
             return phRaiseChemical;
-        }
-        else
+        } else
             return phLowerChemical;
     }
-    private double sodaRaisePH(double currentPH, double newPH, int poolCapacity)
-    {
-        List<Double> bracket = Arrays.asList(6.0, 6.1, 6.2,6.3,6.4,6.5,6.6,6.7,6.8,6.9,7.0,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8.0,8.1,8.2,8.3,8.4);
+
+    int i = 0;
+    double total = 0;
+
+    private double sodaRaisePH(double currentPH, double newPH, int poolCapacity) {
+        List<Double> bracket = Arrays.asList(6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4);
         //6.0 to 7.5
-        List<Double> sodaRaise = Arrays.asList(31.0,28.0,25.0,22.0,20.0,17.0,15.0,13.0,12.0,10.0,8.6,7.3,6.2,5.2,4.4);
+        List<Double> sodaRaise = Arrays.asList(31.0, 28.0, 25.0, 22.0, 20.0, 17.0, 15.0, 13.0, 12.0, 10.0, 8.6, 7.3, 6.2, 5.2, 4.4);
+
+
+        for (int i = 14; i >= 0; i--) {
+            if (currentPH >= newPH)
+                return 0;
+            else if (currentPH >= bracket.get(i) && newPH >= bracket.get(i + 1)) {
+                return sodaRaisePH(bracket.get(i + 1), newPH, poolCapacity) + sodaRaise.get(i) * (poolCapacity / sPool);
+            }
+        }
+        return total;
+
+        /*
         if (currentPH >= bracket.get(15))
             throw new IllegalArgumentException("This pH is high not low!");
         if (currentPH >= newPH)
@@ -147,34 +156,33 @@ public class MainActivity extends AppCompatActivity {
 
         else
             throw new IllegalArgumentException("An error has occurred");
+            */
     }
 
     private double bisulfateLowerPH(double currentPH, double newPH, int poolCapacity) {
-        List<Double> bracket = Arrays.asList(6.0, 6.1, 6.2,6.3,6.4,6.5,6.6,6.7,6.8,6.9,7.0,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8.0,8.1,8.2,8.3,8.4);
+        List<Double> bracket = Arrays.asList(6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4);
         //6.0 to 7.5 array
-        List<Double> bisulfateLower = Arrays.asList(2.2,2.0,1.9,1.9,2.0,2.2);
+        List<Double> bisulfateLower = Arrays.asList(2.2, 2.0, 1.9, 1.9, 2.0, 2.2);
         if (currentPH <= bracket.get(10))
             return 0;
         if (currentPH <= newPH)
             return 0;
         else if (currentPH <= bracket.get(19) && newPH <= bracket.get(18))
-            return bisulfateLower.get(5) *(poolCapacity/sPool);
+            return bisulfateLower.get(5) * (poolCapacity / sPool);
         else if (currentPH <= bracket.get(20) && newPH <= bracket.get(19))
-            return bisulfateLowerPH(bracket.get(19), newPH, poolCapacity) + bisulfateLower.get(4) *(poolCapacity/sPool);
+            return bisulfateLowerPH(bracket.get(19), newPH, poolCapacity) + bisulfateLower.get(4) * (poolCapacity / sPool);
         else if (currentPH <= bracket.get(21) && newPH <= bracket.get(20))
-            return bisulfateLowerPH(bracket.get(20), newPH, poolCapacity) + bisulfateLower.get(3) *(poolCapacity/sPool);
+            return bisulfateLowerPH(bracket.get(20), newPH, poolCapacity) + bisulfateLower.get(3) * (poolCapacity / sPool);
         else if (currentPH <= bracket.get(22) && newPH <= bracket.get(21))
-            return bisulfateLowerPH(bracket.get(21), newPH, poolCapacity) + bisulfateLower.get(2) *(poolCapacity/sPool);
+            return bisulfateLowerPH(bracket.get(21), newPH, poolCapacity) + bisulfateLower.get(2) * (poolCapacity / sPool);
         else if (currentPH <= bracket.get(23) && newPH <= bracket.get(22))
-            return bisulfateLowerPH(bracket.get(22), newPH, poolCapacity) + bisulfateLower.get(1) *(poolCapacity/sPool);
+            return bisulfateLowerPH(bracket.get(22), newPH, poolCapacity) + bisulfateLower.get(1) * (poolCapacity / sPool);
         else if (currentPH <= bracket.get(24) && newPH <= bracket.get(23))
-            return bisulfateLowerPH(bracket.get(23), newPH, poolCapacity) + bisulfateLower.get(0) *(poolCapacity/sPool);
+            return bisulfateLowerPH(bracket.get(23), newPH, poolCapacity) + bisulfateLower.get(0) * (poolCapacity / sPool);
 
         else
             throw new IllegalArgumentException("An error has occurred");
     }
-
-
 
 
     private static final double sPool = 10000; //standard pool capacity
